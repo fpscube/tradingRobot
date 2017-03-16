25e6aa5b614c7ccf2155358d8b8aecd4a813281c
PC-STEPHANE <PC-STEPHANE@DESKTOP-VVOQCCC>
2017-03-16 22:26:14 +0100

    first commit

---
+++ src/tradingRobot.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : tradingRobot.cpp
+// Author      : S.KOEHLER
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "" << endl; // prints 
+	return 0;
+}
